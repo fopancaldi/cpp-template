fa02d40b172c92f7f9c44b645e07454e135aad1e
Francesco Orso Pancaldi <francesco.orso.pancaldi@gmail.com>
2024-09-29 13:49:37 +0000

    Split provide-dependencies.sh so that the useless dependencies are not downloaded in CI.

--- .devcontainer/library-scripts/provide-dependencies-local.sh
+++ .devcontainer/library-scripts/provide-dependencies-local.sh
@@ -1,14 +1,15 @@
 # Stop the program at the first error
 set -e
 
-# Install the necessary programs
+# Install the necessary programs for developement
+# The programs listed in provide-dependencies-ci.sh are omitted here
+# therefore both scripts must be run in order to set up the local developement environment
 apt-get install -y \
-    g++ \
-    cmake \
     clang-format \
     git \
     doxygen \
-    graphviz
+    graphviz \
+    gdb
 
 # For generating latex documentation with doxygen, texlive and ghostscript also need to be installed
 # See https://www.doxygen.nl/manual/install.html for more info
